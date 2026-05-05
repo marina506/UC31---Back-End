67c865074ffd730ec7205b9d3a4cd70b95d31af1
marina506 <marinaoligomes53@gmail.com>
2026-05-05 08:20:06 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,13 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/usuário')
+def usuario():
+    usuario = {'Nome:''Marina', 'Email:' 'marinaoligomes53@gmail.com'}
+    return render_template('index.html', title='Página inicial',
+     usuario=usuario)
+
 @app.route('/contato')
 def contato():
-    return 'Tela de contato'
-
+    nome = "Marina"
+    return render_template('index.html', title='Página inicial', nome=nome)
+    
 if __name__ == '__main__':
     app.run()
