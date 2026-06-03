31eed6d227ce7313704922583d152873f0b753e1
marina506 <marinaoligomes53@gmail.com>
2026-06-03 08:26:26 -0300

    Add files via upload

--- cadastro.py
+++ cadastro.py
@@ -15,5 +15,21 @@ def cadastro():
            mensagem = f"O cadastro foi realizado com sucesso! Bem-Vindo, {nome}"
     return render_template('cadastro.html', mensagem=mensagem)  
 
+@app.route('/')
+def formulario():
+    return render_template('index.html')
+
+@app.route('/validacao', methods=['POST'])
+def cadastro():
+    nome = request.form.get('nome', '').strip().title()
+    email = request.form.get('email', '').strip().lower()
+    cidade = request.form.get('cidade', '').strip().title()
+
+    return f"""
+    Nome: {nome}<br>
+    Email: {email}<br>
+    Cidade: {cidade}
+    """
+
 if __name__=='__main__':
     app.run(debug=True)     
